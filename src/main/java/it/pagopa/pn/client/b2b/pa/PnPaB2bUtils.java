28df61a67f719d1b9612a7993715cadd21a1fe6b
marco-vit-pagopa <marco.vit@pagopa.it>
2022-07-08 18:48:14 +0200

    Attivazione test

--- src/main/java/it/pagopa/pn/client/b2b/pa/PnPaB2bUtils.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/PnPaB2bUtils.java
@@ -3,14 +3,9 @@ package it.pagopa.pn.client.b2b.pa;
 import it.pagopa.pn.client.b2b.pa.generated.openapi.clients.externalb2bpa.model.*;
 import it.pagopa.pn.client.b2b.pa.impl.IPnPaB2bClient;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.LaxRedirectStrategy;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
@@ -24,7 +19,6 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
@@ -38,10 +32,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class PnPaB2bUtils {
+
+    private static Logger log = LoggerFactory.getLogger(PnPaB2bUtils.class);
 
     private final RestTemplate restTemplate;
     private final ApplicationContext ctx;
@@ -61,35 +56,37 @@ public class PnPaB2bUtils {
     }
 
 
-    public void uploadNotification( NewNotificationRequest request) throws IOException {
+    public NewNotificationResponse uploadNotification( NewNotificationRequest request) throws IOException {
 
         List<NotificationDocument> newdocs = new ArrayList<>();
-        for (NotificationDocument doc:
-             request.getDocuments()) {
+        for (NotificationDocument doc : request.getDocuments()) {
             newdocs.add(this.preloadDocument(doc));
         }
         request.setDocuments(newdocs);
 
-        for( NotificationRecipient recipient: request.getRecipients() ) {
-
+        for (NotificationRecipient recipient : request.getRecipients()) {
             NotificationPaymentInfo paymentInfo = recipient.getPayment();
-            paymentInfo.setPagoPaForm( preloadAttachment( paymentInfo.getPagoPaForm() ));
-            paymentInfo.setF24flatRate( preloadAttachment( paymentInfo.getF24flatRate() ));
-            paymentInfo.setF24standard( preloadAttachment( paymentInfo.getF24standard() ));
+            paymentInfo.setPagoPaForm(preloadAttachment(paymentInfo.getPagoPaForm()));
+            paymentInfo.setF24flatRate(preloadAttachment(paymentInfo.getF24flatRate()));
+            paymentInfo.setF24standard(preloadAttachment(paymentInfo.getF24standard()));
         }
 
-
+        log.info("New Notification Request {}", request);
         NewNotificationResponse response = client.sendNewNotification( request );
-        System.out.println( response );
+        log.info("New Notification Request response {}", response);
+        return response;
+    }
 
 
-        System.out.println("Request status for " + response.getNotificationRequestId() );
+    public FullSentNotification waitForRequestAcceptation( NewNotificationResponse response) {
+
+        log.info("Request status for " + response.getNotificationRequestId() );
         NewNotificationRequestStatusResponse status = null;
         for( int i = 0; i < 10; i++ ) {
 
             status = client.getNotificationRequestStatus( response.getNotificationRequestId() );
 
-            System.out.println( status.getNotificationRequestStatus());
+            log.info("New Notification Request status {}", status.getNotificationRequestStatus());
             if ( "ACCEPTED".equals( status.getNotificationRequestStatus() )) {
                 break;
             }
@@ -99,55 +96,74 @@ public class PnPaB2bUtils {
             } catch (InterruptedException exc) {
                 throw new RuntimeException( exc );
             }
-
         }
 
-
         String iun = status.getIun();
-        FullSentNotification fsn = client.getSentNotification( iun );
+        return client.getSentNotification( iun );
+    }
 
-        for (NotificationDocument doc :
-                fsn.getDocuments()) {
+    public void verifyNotification(FullSentNotification fsn) throws IOException, IllegalStateException {
 
-            var resp = client.getSentNotificationDocument(fsn.getIun(), new BigDecimal(doc.getDocIdx()));
+        for (NotificationDocument doc: fsn.getDocuments()) {
+
+            NotificationAttachmentDownloadMetadataResponse resp = client.getSentNotificationDocument(fsn.getIun(), new BigDecimal(doc.getDocIdx()));
             byte[] content = downloadFile(resp.getUrl());
             String sha256 = computeSha256(new ByteArrayInputStream(content));
-            assert sha256.equals(resp.getSha256());
+
+            if( ! sha256.equals(resp.getSha256()) ) {
+                throw new IllegalStateException("SHA256 differs " + doc.getDocIdx() );
+            };
         }
 
         int i = 0;
         for (NotificationRecipient recipient : fsn.getRecipients()) {
 
-            var resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "PAGOPA");
-            byte[] content = downloadFile(resp.getUrl());
-            String sha256 = computeSha256(new ByteArrayInputStream(content));
-            assert sha256.equals(resp.getSha256());
+            NotificationAttachmentDownloadMetadataResponse resp;
 
-/*
-            resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "F24_FLAT");
-            content = downloadFile(resp.getUrl());
-            sha256 = computeSha256(new ByteArrayInputStream(content));
-            assert sha256.equals(resp.getSha256());
+            resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "PAGOPA");
+            checkAttachment( resp );
 
+            //resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "F24_FLAT");
+            //checkAttachment( resp );
 
-            resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "F24_STANDARD");
-            content = downloadFile(resp.getUrl());
-            sha256 = computeSha256(new ByteArrayInputStream(content));
-            assert sha256.equals(resp.getSha256());
-*/
+            //resp = client.getSentNotificationAttachment(fsn.getIun(), new BigDecimal(i), "F24_STANDARD");
+            //checkAttachment( resp );
+
             i++;
         }
 
-        for (LegalFactsId legalFactsId:
-                fsn.getTimeline().get(0).getLegalFactsIds()) {
-            var resp = client.getLegalFact(fsn.getIun(), LegalFactCategory.SENDER_ACK, URLEncoder.encode(legalFactsId.getKey(), StandardCharsets.UTF_8.toString()) );
+        for ( LegalFactsId legalFactsId: fsn.getTimeline().get(0).getLegalFactsIds()) {
+
+            LegalFactDownloadMetadataResponse resp;
+
+            resp = client.getLegalFact(
+                    fsn.getIun(),
+                    LegalFactCategory.SENDER_ACK,
+                    URLEncoder.encode(legalFactsId.getKey(), StandardCharsets.UTF_8.toString())
+                );
+
             byte[] content = downloadFile(resp.getUrl());
-            String  pdfprefix = new String( Arrays.copyOfRange(content, 0, 10), StandardCharsets.UTF_8);
-            assert pdfprefix.contains("PDF");
+            String  pdfPrefix = new String( Arrays.copyOfRange(content, 0, 10), StandardCharsets.UTF_8);
+            if( ! pdfPrefix.contains("PDF") ) {
+                throw new IllegalStateException("LegalFact is not a PDF " + legalFactsId );
+            }
         }
 
+        if(
+                fsn.getNotificationStatus() == null
+             ||
+                fsn.getNotificationStatus().equals( NotificationStatus.REFUSED )
+        ) {
+            throw new IllegalStateException("WRONG STATUS: " + fsn.getNotificationStatus() );
+        }
+    }
 
-        System.out.println( "Notifica ACCETTATA:" + fsn );
+    private void checkAttachment(NotificationAttachmentDownloadMetadataResponse resp) throws IOException {
+        byte[] content = downloadFile(resp.getUrl());
+        String sha256 = computeSha256(new ByteArrayInputStream(content));
+        if( ! sha256.equals(resp.getSha256()) ) {
+            throw new IllegalStateException("SHA256 differs " + resp.getFilename() );
+        };
     }
 
 
@@ -161,8 +177,8 @@ public class PnPaB2bUtils {
         String secret = preloadResp.getSecret();
         String url = preloadResp.getUrl();
 
-        System.out.format("Attachment resourceKey=%s sha256=%s secret=%s presignedUrl=%s\n",
-                resourceName, sha256, secret, url);
+        log.info(String.format("Attachment resourceKey=%s sha256=%s secret=%s presignedUrl=%s\n",
+                resourceName, sha256, secret, url));
 
         loadToPresigned( url, secret, sha256, resourceName );
 
@@ -184,8 +200,8 @@ public class PnPaB2bUtils {
             String secret = preloadResp.getSecret();
             String url = preloadResp.getUrl();
 
-            System.out.format("Attachment resourceKey=%s sha256=%s secret=%s presignedUrl=%s\n",
-                    resourceName, sha256, secret, url);
+            log.info(String.format("Attachment resourceKey=%s sha256=%s secret=%s presignedUrl=%s\n",
+                    resourceName, sha256, secret, url));
 
             loadToPresigned( url, secret, sha256, resourceName );
 
--- src/test/java/it/pagopa/pn/client/b2b/pa/NewNotificationTest.java
+++ src/test/java/it/pagopa/pn/client/b2b/pa/NewNotificationTest.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 @SpringBootTest( classes = {
         PnPaB2bExternalClientImpl.class,
         PnPaB2bInternalClientImpl.class,
@@ -26,8 +29,11 @@ public class NewNotificationTest {
 
     @Test
     public void insertNewNotification() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
         NewNotificationRequest request = new NewNotificationRequest()
-                .subject("Subject")
+                .subject("Test inserimento " + dateFormat.format(calendar.getTime()))
                 .cancelledIun("")
                 .group("")
                 ._abstract("Abstract della notifica")
@@ -37,10 +43,15 @@ public class NewNotificationTest {
                 .physicalCommunicationType( NewNotificationRequest.PhysicalCommunicationTypeEnum.REGISTERED_LETTER_890 )
                 .paProtocolNumber("" + System.currentTimeMillis())
                 .addDocumentsItem( newDocument( "classpath:/sample.pdf" ) )
-                .addRecipientsItem( newRecipient( "Ale", "MNZLSN99E05F205J","classpath:/sample.pdf"))
+                .addRecipientsItem( newRecipient( "Fiera", "FRMTTR76M06B715E","classpath:/sample.pdf"))
                 ;
 
-        Assertions.assertDoesNotThrow(() -> utils.uploadNotification( request ));
+        Assertions.assertDoesNotThrow(() -> {
+            NewNotificationResponse newNotificationRequest = utils.uploadNotification( request );
+            FullSentNotification newNotification = utils.waitForRequestAcceptation( newNotificationRequest );
+            Thread.sleep( 10 * 1000);
+            utils.verifyNotification( newNotification );
+        });
     }
 
 
@@ -57,12 +68,14 @@ public class NewNotificationTest {
     }
 
     private NotificationRecipient newRecipient(String prefix, String taxId, String resourcePath ) {
-        return new NotificationRecipient()
+        long epochMillis = System.currentTimeMillis();
+
+        NotificationRecipient recipient = new NotificationRecipient()
                 .denomination( prefix + " denomination")
                 .taxId( taxId )
                 .digitalDomicile( new NotificationDigitalAddress()
                         .type(NotificationDigitalAddress.TypeEnum.PEC)
-                        .address( "testpagopa2@pnpagopa.postecert.local")
+                        .address( "FRMTTR76M06B715E@pnpagopa.postecert.local")
                 )
                 .physicalAddress( new NotificationPhysicalAddress()
                         .address("Via senza nome")
@@ -74,12 +87,21 @@ public class NewNotificationTest {
                 .recipientType( NotificationRecipient.RecipientTypeEnum.PF )
                 .payment( new NotificationPaymentInfo()
                                 .creditorTaxId("77777777777")
-                                .noticeCode( String.format("30201%13d", System.currentTimeMillis()) )
-                                .noticeCodeOptional( String.format("30201%13d", System.currentTimeMillis()) )
+                                .noticeCode( String.format("30201%13d", epochMillis ) )
+                                .noticeCodeOptional( String.format("30201%13d", epochMillis+1 ) )
                                 .pagoPaForm( newAttachment( resourcePath ))
                         //.f24flatRate( newAttachment( resourcePath ) )
                         //.f24standard( newAttachment( resourcePath ) )
                 );
+
+        try {
+            Thread.sleep(10);
+        }
+        catch (InterruptedException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        return recipient;
     }
 
 }
