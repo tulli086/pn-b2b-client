2172f4c9c93b6b7f3df025daecb3869fe68ea5ff
Christian Fiorillo <c.fiorillo@mediaticaspa.it>
2022-12-14 09:36:04 +0100

    PN-2848 added test timeline

--- src/main/java/it/pagopa/pn/client/b2b/pa/testclient/PnSafeStorageInfoExternalClientImpl.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/testclient/PnSafeStorageInfoExternalClientImpl.java
@@ -20,6 +20,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
