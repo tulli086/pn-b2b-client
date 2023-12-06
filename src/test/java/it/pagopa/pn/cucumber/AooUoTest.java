9dd9fe973b9164e829a2a9dab6f419522afa72e6
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-12-06 14:42:32 +0100

    resolve conflict

--- src/test/java/it/pagopa/pn/cucumber/AooUoTest.java
+++ src/test/java/it/pagopa/pn/cucumber/AooUoTest.java
@@ -12,17 +12,11 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
                 "html:target/cucumber-report.html"),
-<<<<<<<< HEAD:src/test/java/it/pagopa/pn/cucumber/AppIoTest.java
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
-})
-@IncludeTags({"appIo"})
-public class AppIoTest {
-========
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps")
 })
 @ExcludeTags({"ignore"})
 @IncludeTags({"AOO_UO"})
 public class AooUoTest {
->>>>>>>> develop:src/test/java/it/pagopa/pn/cucumber/AooUoTest.java
+
 }
