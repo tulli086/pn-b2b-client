6ff32d0e7e68ffc833b5377993fa5e65d1db6291
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-02-15 16:11:35 +0100

    update nrt

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_hotfix.java
@@ -16,7 +16,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 })
 @ExcludeTags({"ignore","uat","appIo","realNormalizzatore"})
 @IncludeTags({"workflowDigitale", "workflowAnalogico", "pagamentiMultipli",
-        "costoAnalogico","Async", "f24", "version","AOO_UO", "Annullamento",
+        "Async", "f24", "version","AOO_UO", "Annullamento",
         "validation", "RetentionAllegati", "apiKeyManager", "downtimeLogs",
         "legalFact", "letturaDestinatario","radd","20Grammi"})
 public class NrtTest_hotfix {
