0edc77a2dde3772dbf846c35eee613481a51fb43
Marco Meloni <marco.meloni@cinetix-team.it>
2024-02-15 13:03:24 +0100

    fix run webhook

--- src/test/java/it/pagopa/pn/cucumber/WebhookTest.java
+++ src/test/java/it/pagopa/pn/cucumber/WebhookTest.java
@@ -15,6 +15,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
 @ExcludeTags({"ignore"})
-@IncludeTags({"webhook1","webhook2"})
+@IncludeTags({"webhook1"})
 public class WebhookTest {
 }
