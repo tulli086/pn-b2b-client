c5c8f3075cd8231fe38910b068f7158fc4fe5153
angelo.minisci <angelo.minisci@gmail.com>
2024-02-19 20:56:02 +0100

    Fix iun

--- src/test/java/it/pagopa/pn/cucumber/steps/pa/RaddAltSteps.java
+++ src/test/java/it/pagopa/pn/cucumber/steps/pa/RaddAltSteps.java
@@ -117,7 +117,7 @@ public class RaddAltSteps {
                 recipientType,
                 null,
                 iun.equalsIgnoreCase("corretto")?sharedSteps.getIunVersionamento():
-                        iun.equalsIgnoreCase("erratto")?"UGYD-XHEZ-KLRM-202402-H-A":null);
+                        iun.equalsIgnoreCase("erratto")?"IUN_GLDZ-MGZD-AGAR-202402-Y-1":null);
 
         log.info("actInquiryResponse: {}", actInquiryResponse);
         this.actInquiryResponse = actInquiryResponse;
