c63626123c8ec56a1396b3bf6c8d2cbdd18867c0
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-03-04 11:12:46 +0100

    mod suite and restTemplate

--- src/main/java/it/pagopa/pn/client/b2b/pa/springconfig/RestTemplateConfiguration.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/springconfig/RestTemplateConfiguration.java
@@ -1,6 +1,8 @@
 package it.pagopa.pn.client.b2b.pa.springconfig;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -26,15 +28,22 @@ public class RestTemplateConfiguration {
 
     public static final String CUCUMBER_SCENARIO_NAME_MDC_ENTRY = "cucumber_scenario_name";
 
+    private static final int MAX_CONN_PER_ROUTE = 10; //DEFAULT 5
+    private static final int MAX_CONN_TOTAL = 20; //DEFAULT 10
+
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(990_000);
-        requestFactory.setReadTimeout(990_000);
-        requestFactory.setConnectionRequestTimeout(990_000);
+
+        requestFactory.setHttpClient(httpClient());
+
+        requestFactory.setConnectTimeout(70_000);
+        requestFactory.setReadTimeout(70_000);
+        requestFactory.setConnectionRequestTimeout(70_000);
         requestFactory.setBufferRequestBody(false);
+
         restTemplate.setRequestFactory(requestFactory);
 
         List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
@@ -47,6 +56,14 @@ public class RestTemplateConfiguration {
         return restTemplate;
     }
 
+    private HttpClient httpClient() {
+        HttpClientBuilder httpClientBuilder =
+                HttpClientBuilder.create()
+                        .setMaxConnPerRoute(MAX_CONN_PER_ROUTE)
+                        .setMaxConnTotal(MAX_CONN_TOTAL);
+
+        return httpClientBuilder.build();
+    }
 
     private static class RequestAndTraceIdInterceptor implements ClientHttpRequestInterceptor {
 
