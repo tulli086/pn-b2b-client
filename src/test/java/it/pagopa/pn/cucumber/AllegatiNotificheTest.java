332b31f4dde42208209eb6668fe63593cc9b6cce
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-03-06 19:05:12 +0100

    refactor feature file

--- src/test/java/it/pagopa/pn/cucumber/AllegatiNotificheTest.java
+++ src/test/java/it/pagopa/pn/cucumber/AllegatiNotificheTest.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("it/pagopa/pn/cucumber/Allegati.feature")
+@SelectClasspathResource("it/pagopa/pn/cucumber/baseFunction/Allegati.feature")
 @ConfigurationParameters({
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
