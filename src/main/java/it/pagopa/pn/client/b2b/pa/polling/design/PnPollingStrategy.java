f308a6aac5a21bff7c23c53d2a2a069dee510078
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2024-03-07 17:12:37 +0100

    Updated polling pattern implementation draft

--- src/main/java/it/pagopa/pn/client/b2b/pa/polling/design/PnPollingStrategy.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/polling/design/PnPollingStrategy.java
@@ -3,7 +3,7 @@ package it.pagopa.pn.client.b2b.pa.polling.design;
 
 public class PnPollingStrategy {
     public static final String TIMELINE_RAPID_NEW_VERSION = "TIMELINE_RAPID_NEW_VERSION";
-//    public static final String TIMELINE_SLOW_OLD_VERSION = "TIMELINE_SLOW";
+    public static final String TIMELINE_SLOW_OLD_VERSION = "TIMELINE_SLOW_OLD_VERSION";
 //    public static final String VERSION_10 = "VERSION_10";
 //    public static final String VERSION_23 = "VERSION_23";
 }
--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPollingServiceTimelineSlowOldVersion.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPollingServiceTimelineSlowOldVersion.java
@@ -1,28 +1,29 @@
 package it.pagopa.pn.client.b2b.pa.service.impl;
 
-import it.pagopa.pn.client.b2b.pa.generated.openapi.clients.externalb2bpa.model.FullSentNotificationV23;
+import it.pagopa.pn.client.b2b.pa.generated.openapi.clients.externalb2bpa.model.FullSentNotification;
+import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingStrategy;
 import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingTemplate;
 import it.pagopa.pn.client.b2b.pa.polling.dto.PnPollingResponse;
-
+import org.springframework.stereotype.Service;
 import java.util.function.Predicate;
 
 
-//@Service(PnPollingStrategy.TIMELINE_SLOW)
-public class PnPollingServiceTimelineSlowOldVersion extends PnPollingTemplate<PnPollingResponse<FullSentNotificationV23>> {
+@Service(PnPollingStrategy.TIMELINE_SLOW_OLD_VERSION)
+public class PnPollingServiceTimelineSlowOldVersion extends PnPollingTemplate<PnPollingResponse<FullSentNotification>> {
 
     @Override
-    public PnPollingResponse<FullSentNotificationV23> waitForEvent(String iun, String value) {
+    public PnPollingResponse<FullSentNotification> waitForEvent(String iun, String value) {
         return null;
     }
 
     @Override
-    protected Predicate<PnPollingResponse<FullSentNotificationV23>> checkCondition(String iun, String value) {
+    protected Predicate<PnPollingResponse<FullSentNotification>> checkCondition(String iun, String value) {
         //Example use v1 for check
         return null;
     }
 
     @Override
-    protected PnPollingResponse<FullSentNotificationV23> getPollingResponse(String iun, String value) {
+    protected PnPollingResponse<FullSentNotification> getPollingResponse(String iun, String value) {
         return null;
     }
 }
