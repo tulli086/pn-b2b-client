bcc6ab66a5da4b4de13fd83b5c44dbc3b4facb85
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2024-03-07 17:39:02 +0100

    Updated polling pattern implementation draft

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPollingServiceTimelineRapidNewVersion.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/impl/PnPollingServiceTimelineRapidNewVersion.java
@@ -3,27 +3,28 @@ package it.pagopa.pn.client.b2b.pa.service.impl;
 import it.pagopa.pn.client.b2b.pa.PnPaB2bUtils;
 import it.pagopa.pn.client.b2b.pa.generated.openapi.clients.externalb2bpa.model.FullSentNotificationV23;
 import it.pagopa.pn.client.b2b.pa.generated.openapi.clients.externalb2bpa.model.TimelineElementV23;
-import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingTemplate;
-import it.pagopa.pn.client.b2b.pa.polling.mapper.PnGenericFullSentNotificationMapper;
-import it.pagopa.pn.client.b2b.pa.polling.dto.PnPollingResponse;
 import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingStrategy;
+import it.pagopa.pn.client.b2b.pa.polling.design.PnPollingTemplate;
+import it.pagopa.pn.client.b2b.pa.polling.dto.PnPollingResponse;
 import it.pagopa.pn.client.b2b.pa.utils.TimingForTimeline;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.function.Predicate;
 
 
+@Slf4j
 @AllArgsConstructor
 @Service(PnPollingStrategy.TIMELINE_RAPID_NEW_VERSION)
-public class PnPollingServiceTimelineRapidNewVersion extends PnPollingTemplate<PnPollingResponse> {
+public class PnPollingServiceTimelineRapidNewVersion extends PnPollingTemplate<PnPollingResponse<FullSentNotificationV23>> {
     private final TimingForTimeline timingForTimeline;
     private final PnPaB2bUtils pnPaB2bUtils;
-    private final PnGenericFullSentNotificationMapper pnGenericFullSentNotificationMapper;
 
 
     @Override
-    protected Predicate<PnPollingResponse> checkCondition(String iun, String value) {
+    protected Predicate<PnPollingResponse<FullSentNotificationV23>> checkCondition(String iun, String value) {
         return (pnPollingResponse) -> {
             List<TimelineElementV23> isPresentCategory = pnPollingResponse
                     .getPnGenericFullSentNotification()
@@ -45,24 +46,24 @@ public class PnPollingServiceTimelineRapidNewVersion extends PnPollingTemplate<P
     }
 
     @Override
-    public PnPollingResponse getPollingResponse(String iun, String value) {
+    public PnPollingResponse<FullSentNotificationV23> getPollingResponse(String iun, String value) {
         //Example use v2.3 for check
         FullSentNotificationV23 fullSentNotificationV23 = pnPaB2bUtils.getNotificationByIun(iun);
-        return PnPollingResponse.builder()
-                .pnGenericFullSentNotification(pnGenericFullSentNotificationMapper.mapToGeneric(fullSentNotificationV23))
-                .build();
+        PnPollingResponse<FullSentNotificationV23> pnPollingResponse = new PnPollingResponse<>();
+        pnPollingResponse.setPnGenericFullSentNotification(fullSentNotificationV23);
+        return pnPollingResponse;
     }
 
     @Override
-    public PnPollingResponse waitForEvent(String iun, String value) {
+    public PnPollingResponse<FullSentNotificationV23> waitForEvent(String iun, String value) {
         return this.initialize(iun, value);
     }
 
-    protected PnPollingResponse initialize(String iun, String value) {
+    protected PnPollingResponse<FullSentNotificationV23> initialize(String iun, String value) {
         TimingForTimeline.TimingResult timingResult = timingForTimeline.getTimingForElement(value);
         super.pollDelay = 0;
         super.atMost = timingResult.waiting();
-        super.pollDelay = timingResult.waiting() * timingResult.numCheck();
+        super.pollInterval = timingResult.waiting() * timingResult.numCheck();
         return super.initialize(iun, value);
     }
 }
