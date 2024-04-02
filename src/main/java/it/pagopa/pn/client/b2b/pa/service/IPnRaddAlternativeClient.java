516d998d3b919ce068e6ead98eceef9cd7e4551b
Marco Meloni <marco.meloni@cinetix-team.it>
2024-04-02 15:20:07 +0200

    aggiunta metodi e api di ottimizzazione anagrafiche radd

--- src/main/java/it/pagopa/pn/client/b2b/pa/service/IPnRaddAlternativeClient.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/service/IPnRaddAlternativeClient.java
@@ -1,8 +1,12 @@
 package it.pagopa.pn.client.b2b.pa.service;
 
 import it.pagopa.pn.client.b2b.pa.service.utils.SettableAuthTokenRadd;
+import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.externalb2braddalt.model_AnagraficaCsv.RequestResponse;
+import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.externalb2braddalt.model_AnagraficaCsv.VerifyRequestResponse;
 import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.privateb2braddalt.model.*;
 import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.externalb2braddalt.model.*;
+import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.externalb2braddalt.model_AnagraficaCsv.*;
+import it.pagopa.pn.client.b2b.radd.generated.openapi.clients.externalb2braddalt.model_AnagraficaCRUD.*;
 import org.springframework.web.client.RestClientException;
 
 public interface IPnRaddAlternativeClient extends SettableAuthTokenRadd {
@@ -23,5 +27,12 @@ public interface IPnRaddAlternativeClient extends SettableAuthTokenRadd {
     OperationsResponse getAorPracticesByIun(String iun) throws RestClientException;
     OperationAorResponse getAorTransactionByOperationId(String transactionId) throws RestClientException;
     byte[] documentDownload(String operationType, String operationId, String attachmentId) throws RestClientException;
+    RequestResponse retrieveRequestItems(String uid, String requestId, Integer limit, String lastKey) throws RestClientException;
+    RegistryUploadResponse uploadRegistryRequests(String uid, RegistryUploadRequest registryUploadRequest) throws RestClientException;
+    VerifyRequestResponse verifyRequest(String uid, String requestId) throws RestClientException;
+    CreateRegistryResponse addRegistry(String uid, CreateRegistryRequest createRegistryRequest) throws RestClientException;
+    void deleteRegistry(String uid, String registryId, String endDate) throws RestClientException;
+    RegistriesResponse retrieveRegistries(String uid, Integer limit, String lastKey, String externalCode) throws RestClientException;
+    void updateRegistry(String uid, String registryId, UpdateRegistryRequest updateRegistryRequest) throws RestClientException;
 
-    }
+}
