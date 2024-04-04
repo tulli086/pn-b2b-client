f069da38b95510b44173d7d3af8bd355399603e7
EmanueleBonofiglio <emanuele.bonofiglio@pagopa.it>
2024-04-04 12:49:53 +0200

    update pollingFactory

--- src/main/java/it/pagopa/pn/client/b2b/pa/polling/design/PnPollingFactory.java
+++ src/main/java/it/pagopa/pn/client/b2b/pa/polling/design/PnPollingFactory.java
@@ -3,11 +3,14 @@ package it.pagopa.pn.client.b2b.pa.polling.design;
 import it.pagopa.pn.client.b2b.pa.polling.dto.PnPollingParameter;
 import it.pagopa.pn.client.b2b.pa.polling.dto.PnPollingResponse;
 import it.pagopa.pn.client.b2b.pa.polling.IPnPollingService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import java.util.Map;
 
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PnPollingFactory {
     private final Map<String, IPnPollingService<?>> pollingServiceMap;
 
