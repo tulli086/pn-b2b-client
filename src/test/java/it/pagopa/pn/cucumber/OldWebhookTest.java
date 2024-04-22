f11f1167e0a967e1376642b3dc1da135ba3cb156
angelo.minisci <angelo.minisci@gmail.com>
2024-04-22 15:13:36 +0200

    Remove cocurrent

--- src/test/java/it/pagopa/pn/cucumber/OldWebhookTest.java
+++ src/test/java/it/pagopa/pn/cucumber/OldWebhookTest.java
@@ -12,7 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
                 "html:target/cucumber-report.html"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
-        @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
+      //  @ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
 @ExcludeTags({"ignore"})
 @IncludeTags({"oldWebhookTest"})
