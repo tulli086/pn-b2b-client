5d96f4eeb685488b6beaaa7c565db096c29ac40a
Marco Meloni <marco.meloni@cinetix-team.it>
2024-05-10 09:37:00 +0200

    rimmozione 20 grammi e aggiunta radd alt

--- src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
+++ src/test/java/it/pagopa/pn/cucumber/NrtTest_test.java
@@ -19,6 +19,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeTags({"workflowDigitale", "workflowAnalogico", "pagamentiMultipli","giacenza890Simplified",
             "Async", "f24", "version","AOO_UO", "Annullamento",
             "validation", "RetentionAllegati", "apiKeyManager", "downtimeLogs",
-            "legalFact", "letturaDestinatario","20grammi"}) //TMP EXCLUDED: ,"partitaIva","raddAlt"
+            "legalFact", "letturaDestinatario","raddAlt","raddAnagrafica","raddAttoIntero"}) //TMP EXCLUDED: ,"partitaIva"
 public class NrtTest_test {
 }
