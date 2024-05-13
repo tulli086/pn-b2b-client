3cd21fb38e9672dfb37f2e051035d5a774643429
Marco Meloni <marco.meloni@cinetix-team.it>
2024-05-13 17:10:14 +0200

    rimozione configurazione duplica

--- src/test/java/it/pagopa/pn/cucumber/CallCenterEvolutoTest.java
+++ src/test/java/it/pagopa/pn/cucumber/CallCenterEvolutoTest.java
@@ -13,7 +13,6 @@ import static io.cucumber.junit.platform.engine.Constants.*;
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
                 "html:target/cucumber-report.html"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.pn.cucumber.steps"),
         //@ConfigurationParameter(key = EXECUTION_MODE_FEATURE_PROPERTY_NAME, value = "concurrent"),
 })
 @IncludeTags({"CallCenterEvoluto"})
