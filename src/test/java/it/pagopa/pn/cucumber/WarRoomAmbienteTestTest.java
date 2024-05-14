5f5f398ba4588620f85495f20f600b76268ad2b8
Marco Meloni <marco.meloni@cinetix-team.it>
2024-05-14 16:22:09 +0200

    fix run e2e test

--- src/test/java/it/pagopa/pn/cucumber/WarRoomAmbienteTestTest.java
+++ src/test/java/it/pagopa/pn/cucumber/WarRoomAmbienteTestTest.java
@@ -6,7 +6,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("it/pagopa/pn/cucumber/workflowNotifica/e2e")
+@SelectClasspathResource("it/pagopa/pn/cucumber")
 @ConfigurationParameters({
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty") ,
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-report.json," +
